493ba982c2728708576850f1160e9d17bd7ffeaf
Prashant-17-11 <prashantsingh.999000@gmail.com>
2022-01-16 16:05:49 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import Wrapper from "./Components/Wrapper";
 import Screen from "./Components/Screen";
 import ButtonBox from "./Components/ButtonBox";
@@ -11,10 +13,122 @@ const btnValues = [
   [".", 0, "="],
 ];
 
+const toLocaleString = (num) =>
+  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+
+const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+
 function App() {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    if (removeSpaces(calc.num).length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : removeSpaces(calc.num) % 1 === 0
+            ? toLocaleString(Number(removeSpaces(calc.num + value)))
+            : toLocaleString(calc.num + value),
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  const decimalClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
+  const signClickHandler = (e) => {
+    e.preventDefault();
+    const value = e.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+    });
+  };
+
+  const equalsClickHandler = (e) => {
+    if (calc.sign && calc.num) {
+      const math = (a, b, sign) =>
+        sign === "+"
+          ? a + b
+          : sign === "-"
+          ? a - b
+          : sign === "*"
+          ? a * b
+          : a / b;
+
+      setCalc({
+        ...calc,
+        res:
+          calc.num === "0" && calc.sum === "/"
+            ? "Can't divide with zero"
+            : toLocaleString(
+                math(
+                  Number(removeSpaces(calc.res)),
+                  Number(removeSpaces(calc.num)),
+                  calc.sign
+                )
+              ),
+        sign: "",
+        num: 0,
+      });
+    }
+  };
+
+  const percentClickHanlder = (e) => {
+    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+
+  const invertClickHanlder = (e) => {
+    setCalc({
+      ...calc,
+      num: calc.num ? toLocaleString(removeSpaces(calc.num)) * -1 : 0,
+      res: calc.res ? toLocaleString(removeSpaces(calc.res)) * -1 : 0,
+      sign: "",
+    });
+  };
+
+  const resetClickHandler = (e) => {
+    setCalc({
+      ...calc,
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+
+  const screenValue = calc.num ? calc.num : calc.res;
+
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={screenValue} />
       <ButtonBox>
         {btnValues.flat().map((btn, i) => {
           return (
@@ -22,9 +136,21 @@ function App() {
               key={i}
               className={btn === "=" ? "equals" : ""}
               value={btn}
-              onClick={() => {
-                console.log(`${btn} clicked!`);
-              }}
+              onClick={
+                btn === "C"
+                  ? resetClickHandler
+                  : btn === "+-"
+                  ? invertClickHanlder
+                  : btn === "%"
+                  ? percentClickHanlder
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "*" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : btn === "."
+                  ? decimalClickHandler
+                  : numClickHandler
+              }
             />
           );
         })}
